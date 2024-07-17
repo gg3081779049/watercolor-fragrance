2aefe37be119e3366439ff9681ad4b3a8195885e
gg3081779049 <gg3081779049@163.com>
2024-07-17 12:54:34 +0800

    update

--- src/layout/components/AppMain/index.vue
+++ src/layout/components/AppMain/index.vue
@@ -2,7 +2,7 @@
   <section>
     <router-view v-slot="{ Component }">
       <transition name="fade">
-        <keep-alive :include="cachesTabs">
+        <keep-alive :include="showTabs ? cachesTabs : []">
           <component :is="Component" />
         </keep-alive>
       </transition>
@@ -11,13 +11,15 @@
 </template>
 
 <script>
+import { useSettingsStore } from '@/store/modules/settings'
 import { useTabsStore } from '@/store/modules/tabs'
 import { mapState } from 'pinia'
 
 export default {
   name: "AppMain",
   computed: {
-    ...mapState(useTabsStore, ["cachesTabs"])
+    ...mapState(useTabsStore, ["cachesTabs"]),
+    ...mapState(useSettingsStore, ["showTabs"])
   }
 }
 </script>
